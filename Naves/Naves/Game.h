09662b8dc9c53be60436ec9f548a319ed67b2d72
Alex Florez <UO264371@uniovi.es>
2020-09-19 11:54:11 +0200

    Añadiendo motor del juego

---
+++ Naves/Naves/Game.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// Librerías SDL y otras utilidades
+#include <iostream>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+using namespace std;
+
+// Valores generales
+#define WIDTH 480
+#define HEIGHT 320
+#define FPS 30
+
+class Game
+{
+public:
+	Game();
+	void loop();
+	SDL_Window* window; // ventana
+	SDL_Renderer* renderer; // renderizador
+	bool loopActive; // Juego activo?
+};
+
